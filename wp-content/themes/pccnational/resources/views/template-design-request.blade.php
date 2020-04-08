7a663e99fa0a22884280f595101425dc9ffed8e2
Adam Shortsleeves <ashortsleeves@gmail.com>
2020-04-07 20:00:16 -0400

    design request form

---
+++ wp-content/themes/pccnational/resources/views/template-design-request.blade.php
@@ -0,0 +1,35 @@
+{{--
+  Template Name: Design Request Template
+--}}
+
+@extends('layouts.app')
+
+@section('content')
+  @while(have_posts()) @php the_post() @endphp
+    @include('partials.page-header')
+    @php
+      //page template
+      $role = $get_user_role;
+      global $current_user; wp_get_current_user();
+    @endphp
+
+    @if($role == "subscriber")
+      <section class="page-content page-form">
+        <div class="container container-sm">
+            @php the_content() @endphp
+        </div>
+      </section>
+    @else
+      <section class="page-content not-logged jumbo-bg" style="background-image: url({{$hero['global_background']['url']}})">
+        <div class="container container-sm">
+          <div class="not-logged-content">
+            <h1>ACCESS DENIED</h1>
+            <h2>You must be a PCC National Cabinet partner to access this page</h2>
+            <a class="btn btn-lg" href="/login">Login</a> <a class="btn btn-secondary btn-lg" href="/join-us">Join Us</a>
+          </div>
+        </div>
+      </section>
+
+    @endif
+  @endwhile
+@endsection
